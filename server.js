22baac911366af8e34485ebf83b455c6160523c3
Vivek Yadav <vivek.yadav1909@gmail.com>
2015-10-30 12:08:19 -0800

    unncesessary changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/about', function (req, res){
 	res.send("About Us !");
 });
 app.get('/', function (req, res){
-	res.send("Hello Express");
+	res.send("Hey asshole how are u");
 });
 app.use(express.static(__dirname + '/public'));
 app.listen(PORT, function(){
